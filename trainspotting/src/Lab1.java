73513639a4d3e9cdabd6b7385e0fc5c3e2f05862
maxarfvidssonnilsson <31478176+maxarfvidssonnilsson@users.noreply.github.com>
2019-02-08 11:01:04 +0100

    Revert "Revert "best""
    This reverts commit 0d668493bde9356eb2fb4a16c3d040f834942be2.

--- trainspotting/src/Lab1.java
+++ trainspotting/src/Lab1.java
@@ -9,10 +9,11 @@ import static java.util.Arrays.asList;
 
 public class Lab1 {
 
+  //All of the enums below are used purely to make the code more readable and maintainable in many cases removing the need for comments.  
   enum SwitchName {
     NORTH_STATION_SWITCH, SOUTH_STATION_SWITCH, MIDDLE_SWITCH_EAST, MIDDLE_SWITCH_WEST
   }
-
+  
   enum SensorName {
     SOUTH_OF_NORTH_STATION,NORTH_OF_NORTH_STATION, WEST_OF_CROSSING, NORTH_OF_CROSSING, EAST_OF_CROSSING, SOUTH_OF_CROSSING,
     SOUTHWEST_OF_NORTH_STATION_SWITCH, EAST_OF_NORTH_STATION_SWITCH, WEST_OF_NORTH_STATION_SWITCH, SOUTHEAST_OF_SOUTH_STATION_SWITCH,
@@ -22,17 +23,14 @@ public class Lab1 {
   }
 
   enum SemaphoreName {
-    NORTH_STATION, SINGLETRACK_EAST, SINGLETRACK_WEST, SOUTH_STATION, MIDDLE_DUBBLE_TRACK,CROSSING
+    NORTH_STATION, SOUTH_STATION, SINGLETRACK_EAST, SINGLETRACK_WEST, MIDDLE_DUBBLE_TRACK,CROSSING
   }
 
   private Map<SemaphoreName, Semaphore> semaphoreMap = new HashMap<>();
   private Map<List<Integer>, SensorName> sensorMap = new HashMap<>();
   private Map<SwitchName, List<Integer>> switchMap = new HashMap<>();
 
-  private TSimInterface tsi;
-
   public Lab1(int speed1, int speed2) {
-    tsi = TSimInterface.getInstance();
 
     //All the switches.
     switchMap.put(SwitchName.NORTH_STATION_SWITCH, asList(17,7));
@@ -40,7 +38,7 @@ public class Lab1 {
     switchMap.put(SwitchName.MIDDLE_SWITCH_EAST,asList(15,9));
     switchMap.put(SwitchName.MIDDLE_SWITCH_WEST,asList(4,9));
 
-    //All the stations.
+    //All the station sensors.
     sensorMap.put(asList(16,3), SensorName.NORTH_OF_NORTH_STATION);
     sensorMap.put(asList(16,5), SensorName.SOUTH_OF_NORTH_STATION);
     sensorMap.put(asList(16,11), SensorName.NORTH_OF_SOUTH_STATION);
@@ -81,31 +79,29 @@ public class Lab1 {
     semaphoreMap.put(SemaphoreName.SINGLETRACK_WEST, new Semaphore(1));
 
     //The two trains to run on the map.
-    Train train1 = new Train(1,speed1,SensorName.NORTH_OF_NORTH_STATION,tsi,null, false);
-    Train train2 = new Train(2,speed2,SensorName.NORTH_OF_SOUTH_STATION,tsi,SemaphoreName.SOUTH_STATION, true);
+    Train train1 = new Train(1, speed1,TSimInterface.getInstance(), false);
+    Train train2 = new Train(2, speed2, TSimInterface.getInstance(), true);
 
+    // this is to ensure that the station train2 starts at is acquired even though it actually
+    // never has entered the station but spontaneously spawned there.
     semaphoreMap.get(SemaphoreName.SOUTH_STATION).tryAcquire();
 
     train1.start();
     train2.start();
-
   }
 
   class Train extends Thread {
-    boolean direction;
-    private int velocity;
-    int id;
-    TSimInterface tsi;
-    int maxVelocity;
-    SemaphoreName currentSemaphore;
+    private boolean headedNorth;   //used during sensor events to determine the way a train is headed. 
+    private int velocity, maxVelocity;
+    private int id;
+    private TSimInterface tsi;
 
-    Train(int id, int startVelocity, SensorName startSensor, TSimInterface tsi, SemaphoreName semaphoreName, boolean direction) {
+    Train(int id, int startVelocity, TSimInterface tsi, boolean direction) {
       this.id = id;
-      this.maxVelocity = 50;
+      this.maxVelocity = 19;
       setVelocity(startVelocity);
       this.tsi = tsi;
-      this.currentSemaphore = semaphoreName;
-      this.direction = direction;
+      this.headedNorth = direction;
 
       try {
         tsi.setSpeed(id,this.velocity);
@@ -123,18 +119,14 @@ public class Lab1 {
       }
     }
 
-    private void activateBreak() throws CommandException {
-      tsi.setSpeed(id,0);
-    }
-
     private void changeDirection() {
       velocity = -velocity;
-      direction = !direction;
+      headedNorth = !headedNorth;
     }
 
     private void waitAtStation() {
       try {
-        activateBreak();
+        tsi.setSpeed(id,0);
         sleep(1000 + (20 * Math.abs(velocity)));
         changeDirection();
         tsi.setSpeed(id, velocity);
@@ -151,11 +143,11 @@ public class Lab1 {
       tsi.setSwitch(switchMap.get(switchName).get(0),switchMap.get(switchName).get(1),direction);
     }
 
+    //Stops the train and waits for the semaphore to be acquired before proceeding. 
     private void stopUntilPass(SemaphoreName semaphoreName) throws CommandException, InterruptedException {
       Semaphore semaphore = semaphoreMap.get(semaphoreName);
-      activateBreak();
+      tsi.setSpeed(id,0);
       semaphore.acquire();
-      currentSemaphore = semaphoreName;
       tsi.setSpeed(id, velocity);
     }
 
@@ -166,50 +158,45 @@ public class Lab1 {
       }
     }
 
-    private boolean semaphoreHasPermits(SemaphoreName semaphoreName) {
-      Semaphore semaphore = semaphoreMap.get(semaphoreName);
-      boolean hasPermit = semaphore.tryAcquire();
-      currentSemaphore = semaphoreName;
-      return hasPermit;
-    }
-
-    private void handleExitSwitch(SemaphoreName semaphoreName, SwitchName switchName, int direction1, int direction2) throws CommandException{
-      if (semaphoreHasPermits(semaphoreName)) {
+    //Generalisation of the situation when the train is approaching a switch having to choose what way to go.
+    private void choosePath(SemaphoreName semaphoreName, SwitchName switchName, int direction1, int direction2) throws CommandException{
+      boolean permitAcquired = semaphoreMap.get(semaphoreName).tryAcquire();
+      if (permitAcquired) {
         setSwitch(switchName, direction1);
       } else {
         setSwitch(switchName, direction2);
       }
     }
 
+    //The main logic of the train class handling all the sensor events.
     private void manageSensorEvent(SensorEvent event) throws CommandException, InterruptedException {
       boolean isActive = event.getStatus() == SensorEvent.ACTIVE;
       SensorName sensorName = sensorMap.get(asList(event.getXpos(),event.getYpos()));
       if (isActive) {
         switch (sensorName) {
           case NORTH_OF_NORTH_STATION: case SOUTH_OF_NORTH_STATION:
-            if (direction)  waitAtStation();
+            if (headedNorth)  waitAtStation();
             break;
           case NORTH_OF_SOUTH_STATION: case SOUTH_OF_SOUTH_STATION:
-            if (!direction) waitAtStation();
+            if (!headedNorth) waitAtStation();
             break;
           case WEST_OF_CROSSING: case NORTH_OF_CROSSING:
-            if (direction) releasePermit(SemaphoreName.CROSSING);
+            if (headedNorth) releasePermit(SemaphoreName.CROSSING);
             else stopUntilPass(SemaphoreName.CROSSING);
             break;
           case EAST_OF_CROSSING: case SOUTH_OF_CROSSING:
-            if (direction) stopUntilPass(SemaphoreName.CROSSING);
+            if (headedNorth) stopUntilPass(SemaphoreName.CROSSING);
             else releasePermit(SemaphoreName.CROSSING);
             break;
-
           case EAST_OF_NORTH_STATION_SWITCH:
-            if (!direction) {
+            if (!headedNorth) {
               releasePermit(SemaphoreName.NORTH_STATION);
             } else {
-              handleExitSwitch(SemaphoreName.NORTH_STATION,SwitchName.NORTH_STATION_SWITCH,TSimInterface.SWITCH_LEFT,TSimInterface.SWITCH_RIGHT);
+              choosePath(SemaphoreName.NORTH_STATION,SwitchName.NORTH_STATION_SWITCH,TSimInterface.SWITCH_LEFT,TSimInterface.SWITCH_RIGHT);
             }
             break;
           case WEST_OF_NORTH_STATION_SWITCH:
-            if (!direction) {
+            if (!headedNorth) {
               stopUntilPass(SemaphoreName.SINGLETRACK_EAST);
               setSwitch(SwitchName.NORTH_STATION_SWITCH, TSimInterface.SWITCH_RIGHT);
             } else {
@@ -217,24 +204,23 @@ public class Lab1 {
             }
             break;
           case SOUTHWEST_OF_NORTH_STATION_SWITCH:
-            if (!direction) {
+            if (!headedNorth) {
               stopUntilPass(SemaphoreName.SINGLETRACK_EAST);
               setSwitch(SwitchName.NORTH_STATION_SWITCH, TSimInterface.SWITCH_LEFT);
-            }
-            else {
+            } else {
               releasePermit(SemaphoreName.SINGLETRACK_EAST);
             }
             break;
           case WEST_OF_SOUTH_STATION_SWITCH:
-            if (!direction) {
+            if (!headedNorth) {
               System.out.println(semaphoreMap.get(SemaphoreName.SOUTH_STATION).availablePermits());
-              handleExitSwitch(SemaphoreName.SOUTH_STATION,SwitchName.SOUTH_STATION_SWITCH,TSimInterface.SWITCH_LEFT,TSimInterface.SWITCH_RIGHT);
+              choosePath(SemaphoreName.SOUTH_STATION,SwitchName.SOUTH_STATION_SWITCH,TSimInterface.SWITCH_LEFT,TSimInterface.SWITCH_RIGHT);
             } else {
               releasePermit(SemaphoreName.SOUTH_STATION);
             }
             break;
           case EAST_OF_SOUTH_STATION_SWITCH:
-            if (!direction) {
+            if (!headedNorth) {
               releasePermit(SemaphoreName.SINGLETRACK_WEST);
             } else {
               stopUntilPass(SemaphoreName.SINGLETRACK_WEST);
@@ -242,23 +228,22 @@ public class Lab1 {
             }
             break;
           case SOUTHEAST_OF_SOUTH_STATION_SWITCH:
-            if (!direction) {
+            if (!headedNorth) {
               releasePermit(SemaphoreName.SINGLETRACK_WEST);
             } else {
               stopUntilPass(SemaphoreName.SINGLETRACK_WEST);
               setSwitch(SwitchName.SOUTH_STATION_SWITCH, TSimInterface.SWITCH_RIGHT);
             }
             break;
-
           case WEST_OF_MIDDLE_SWITCH_WEST:
-            if (direction) {
-              handleExitSwitch(SemaphoreName.MIDDLE_DUBBLE_TRACK,SwitchName.MIDDLE_SWITCH_WEST,TSimInterface.SWITCH_LEFT,TSimInterface.SWITCH_RIGHT);
+            if (headedNorth) {
+              choosePath(SemaphoreName.MIDDLE_DUBBLE_TRACK,SwitchName.MIDDLE_SWITCH_WEST,TSimInterface.SWITCH_LEFT,TSimInterface.SWITCH_RIGHT);
             } else {
               releasePermit(SemaphoreName.MIDDLE_DUBBLE_TRACK);
             }
             break;
           case EAST_OF_MIDDLE_SWITCH_WEST:
-            if (direction) {
+            if (headedNorth) {
               releasePermit(SemaphoreName.SINGLETRACK_WEST);
             } else {
               stopUntilPass(SemaphoreName.SINGLETRACK_WEST);
@@ -266,23 +251,22 @@ public class Lab1 {
             }
             break;
           case SOUTHEAST_OF_MIDDLE_SWITCH_WEST:
-            if (direction) {
+            if (headedNorth) {
               releasePermit(SemaphoreName.SINGLETRACK_WEST);
             } else {
               stopUntilPass(SemaphoreName.SINGLETRACK_WEST);
               setSwitch(SwitchName.MIDDLE_SWITCH_WEST, TSimInterface.SWITCH_RIGHT);
             }
             break;
-
           case EAST_OF_MIDDLE_SWITCH_EAST:
-            if (!direction) {
-              handleExitSwitch(SemaphoreName.MIDDLE_DUBBLE_TRACK,SwitchName.MIDDLE_SWITCH_EAST,TSimInterface.SWITCH_RIGHT,TSimInterface.SWITCH_LEFT);
+            if (!headedNorth) {
+              choosePath(SemaphoreName.MIDDLE_DUBBLE_TRACK,SwitchName.MIDDLE_SWITCH_EAST,TSimInterface.SWITCH_RIGHT,TSimInterface.SWITCH_LEFT);
             } else {
               releasePermit(SemaphoreName.MIDDLE_DUBBLE_TRACK);
             }
             break;
           case WEST_OF_MIDDLE_SWITCH_EAST:
-            if (!direction) {
+            if (!headedNorth) {
               releasePermit(SemaphoreName.SINGLETRACK_EAST);
             } else {
               stopUntilPass(SemaphoreName.SINGLETRACK_EAST);
@@ -290,7 +274,7 @@ public class Lab1 {
             }
             break;
           case SOUTHWEST_OF_MIDDLE_SWITCH_EAST:
-            if (!direction) {
+            if (!headedNorth) {
               releasePermit(SemaphoreName.SINGLETRACK_EAST);
             } else {
               stopUntilPass(SemaphoreName.SINGLETRACK_EAST);
@@ -306,9 +290,8 @@ public class Lab1 {
       while (true) {
         try {
           tsi.setSpeed(id, this.velocity);
-
         } catch (CommandException e) {
-          e.printStackTrace();    // or only e.getMessage() for the error
+          e.printStackTrace(); 
           System.exit(1);
         }
         while (!this.isInterrupted()) {
@@ -325,5 +308,4 @@ public class Lab1 {
       }
     }
   }
-
 }
