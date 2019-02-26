ecf273484654903c4feea96fae4e79756b2d35fa
Stefan Chan <stefan2846@hotmail.com>
2019-02-26 15:43:16 +0100

    Documenting the code

--- Erlang/CCHAT/client.erl
+++ Erlang/CCHAT/client.erl
@@ -20,48 +20,35 @@ initial_state(Nick, GUIAtom, ServerAtom) ->
         channels = []
     }.
 
-% handle/2 handles each kind of request from GUI
-% Parameters:
-%   - the current state of the client (St)
-%   - request data from GUI
-% Must return a tuple {reply, Data, NewState}, where:
-%   - Data is what is sent to GUI, either the atom `ok` or a tuple {error, Atom, "Error message"}
-%   - NewState is the updated state of the client
-
-% Join channel
-%handle(St, {join, Channel}) ->
-    % TODO: Implement this function
-%    case lists:member(Channel, channels) of
-%      true -> genserver:request(list_to_atom(Channel), {join, self()}),
-%        {reply, ok, St#client_st{channels =
-%        lists:add(Channel, St#client_st.channels) }};
-%      false -> {reply, {error, server_unreachable, "server is not reachable"}, St}
-%    end;
-
+% Join Channel
 handle(St, {join, Channel}) ->
+  % Checks if server is active
   case lists:member(St#client_st.server, registered()) of
+    % If server is active, tries to join the specified channel
     true -> Result = (catch genserver:request(St#client_st.server, {join, Channel, self()})),
       case Result of
         joined -> {reply, ok, St#client_st{channels = lists:append(St#client_st.channels, [Channel])}};
         failed -> {reply, {error, user_already_joined, "Already in channel"}, St}
       end;
-    false -> {reply, {error, server_unreachable, "Server unreachable"}, St}
+    % Server is not active, return error
+    false -> {reply, {error, server_not_reached, "Server unreachable"}, St}
   end;
 
 % Leave channel
 handle(St, {leave, Channel}) ->
-    % TODO: Implement this function
+    % Checks if client is in channel
     case lists:member(Channel, St#client_st.channels) of
-      true -> (catch genserver:request(list_to_atom(Channel), {leave, self()})),
+      % If in channel, tries to leave
+      true -> genserver:request(list_to_atom(Channel), {leave, self()}),
         {reply, ok, St#client_st{channels =
          lists:delete(Channel, St#client_st.channels) }};
+      % If not in channel, returns error
       false -> {reply, {error, user_not_joined, "User not in channel"}, St}
     end;
 
 % Sending message (from GUI, to channel)
 handle(St, {message_send, Channel, Msg}) ->
-    % TODO: Implement this function
-    % {reply, ok, St} ;
+    % Sends request to genserver for messaging all clients in Channel
     Result = (catch genserver:request(list_to_atom(Channel), {message, Channel,
     St#client_st.nick, Msg, self()})),
     case Result of
